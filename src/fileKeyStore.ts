40c1bf622ca9885b098b2bfadebff57ea886dc5b
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-02-05 14:28:27 -0600

    :memo: Document fileKeyStore.ts

--- src/fileKeyStore.ts
+++ src/fileKeyStore.ts
@@ -7,14 +7,21 @@ import {
 } from './baseKeyStore'
 
 /**
- * Persists keys into the file system, needs a path to the file and a password.
- * The password is used to generate a key using scrypt. The store is encrypted
- * with aes-256-gcm. Uses AEAD, grabs the mac address of the first external
- * interface and uses it as the context to lock to the machine.
+ * A implementation of BaseKeyStore that stores the sealed
+ * keys into a desiginated spot in the file system, consumers
+ * of the class supply providers that give the password, salt
+ * and context to use for AAED
  */
 export class FileKeyStore extends BaseKeyStore {
   private readonly keyStorePath: string
 
+  /**
+   *
+   * @param {string} keyStorePath path to the folder where keys will be saved
+   * @param {IKeyStoreValueProvider} keyStorePasswordProvider provide the password used to seal keys
+   * @param {IKeyStoreValueProvider} keyStoreSaltProvider provide the salt used to seal keys
+   * @param {IKeyStoreContextProvider} keyStoreContextProvider provider that will give the appropriate context based on key id
+   */
   constructor(
     keyStorePath: string,
     keyStorePasswordProvider: IKeyStoreValueProvider,
@@ -29,6 +36,9 @@ export class FileKeyStore extends BaseKeyStore {
     this.keyStorePath = resolveHome(keyStorePath)
   }
 
+  /**
+   * @Inheritdoc
+   */
   protected hasKeyInSlot(keySlot: string): Promise<boolean> {
     return access(this.keyStorePath + '/' + keySlot)
       .then(() => true)
@@ -41,23 +51,38 @@ export class FileKeyStore extends BaseKeyStore {
     })
   }
 
+  /**
+   * @Inheritdoc
+   */
   protected async putKeyInSlot(keySlot: string, key: Buffer): Promise<void> {
     await this.createKeyStoreDirIfNotExists()
     await writeFile(this.keyStorePath + '/' + keySlot, key)
   }
 
+  /**
+   * @Inheritdoc
+   */
   protected async getKeyInSlot(keySlot: string): Promise<Buffer> {
     return readFile(this.keyStorePath + '/' + keySlot)
   }
 
+  /**
+   * @Inheritdoc
+   */
   protected async deleteKeySlot(keySlot: string): Promise<void> {
     await unlink(this.keyStorePath + '/' + keySlot)
   }
 
+  /**
+   * @Inheritdoc
+   */
   protected async clearKeySlots(): Promise<void> {
     await rm(this.keyStorePath, { recursive: true, force: true, maxRetries: 3 })
   }
 
+  /**
+   * @Inheritdoc
+   */
   async close(): Promise<void> {
     // nothing to do
   }
